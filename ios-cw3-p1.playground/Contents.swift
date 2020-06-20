96a23a38acbd3f1a5f4d32bd8b909a214fe9d2da
lulwaRashed <looloo--2004@hotmail.com>
2020-06-20 20:24:19 +0300

    DONE

---
+++ ios-cw3-p1.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var number1 = 6
+var number2 = 10
+
+func add(a: Int , b: Int) -> Int
+{
+    
+     var sum = a + b
+    return   sum
+    
+}
+ add(a: number1, b: number2)
+
+print(add(a: number1, b: number2))
